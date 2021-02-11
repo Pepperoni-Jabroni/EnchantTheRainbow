8f7be18a824125d3aa4be2a471e35d1069eabef9
Pepperoni-Jabroni <nickrohn18@gmail.com>
2021-02-10 20:05:33 -0800

    Initial main class

---
+++ src/main/java/pepjebs/enchant_the_rainbow/EnchantTheRainbowMod.java
@@ -0,0 +1,10 @@
+package pepjebs.enchant_the_rainbow;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EnchantTheRainbowMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
