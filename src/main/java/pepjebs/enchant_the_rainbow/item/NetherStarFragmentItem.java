28fa76c6b321b3c475ca0e8a3cdb82f1d887d3ab
Pepperoni-Jabroni <nickrohn18@gmail.com>
2021-12-20 22:03:49 -0800

    Adjust textures & add tooltip for glint color

--- src/main/java/pepjebs/enchant_the_rainbow/item/NetherStarFragmentItem.java
+++ src/main/java/pepjebs/enchant_the_rainbow/item/NetherStarFragmentItem.java
@@ -2,7 +2,11 @@ package pepjebs.enchant_the_rainbow.item;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DyeColor;
 import pepjebs.enchant_the_rainbow.EnchantTheRainbowMod;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class NetherStarFragmentItem extends Item {
     public NetherStarFragmentItem(Settings settings) {
@@ -11,6 +15,17 @@ public class NetherStarFragmentItem extends Item {
 
     @Override
     public boolean hasGlint(ItemStack stack) {
-        return stack.getNbt() != null && stack.getNbt().contains(EnchantTheRainbowMod.GLINT_COLOR_NBT_TAG);
+        return true;
+    }
+
+    public static String getTooltipColorForStack(ItemStack stack){
+        if (stack.getNbt() == null || !stack.getNbt().contains(EnchantTheRainbowMod.GLINT_COLOR_NBT_TAG))
+            return null;
+        int color = stack.getNbt().getInt(EnchantTheRainbowMod.GLINT_COLOR_NBT_TAG);
+        String dyeColor = DyeColor.byId(color).getName();
+        return Arrays.stream(dyeColor
+                        .split("_"))
+                .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
+                .collect(Collectors.joining(" "));
     }
 }
---
+++ src/main/java/pepjebs/enchant_the_rainbow/mixin/ItemMixin.java
@@ -0,0 +1,37 @@
+package pepjebs.enchant_the_rainbow.mixin;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import pepjebs.enchant_the_rainbow.item.NetherStarFragmentItem;
+
+import java.util.List;
+
+@Mixin(Item.class)
+public class ItemMixin {
+
+    @Inject(method = "appendTooltip", at = @At(value = "TAIL"))
+    public void appendFragmentColorTooltip(
+            ItemStack stack,
+            @Nullable World world,
+            List<Text> tooltip,
+            TooltipContext context,
+            CallbackInfo ci) {
+        String tooltipColor = NetherStarFragmentItem.getTooltipColorForStack(stack);
+        if (tooltipColor == null)
+            return;
+        tooltip.add(
+                new TranslatableText("item.enchant_the_rainbow.nether_star_fragment.color", tooltipColor)
+                        .formatted(Formatting.GRAY)
+        );
+    }
+}
